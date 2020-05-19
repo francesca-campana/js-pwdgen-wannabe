91f515c4f05ce802d2b55a61e2ddf79a9e9b4764
francesca-campana <franx3rox@gmail.com>
2020-05-19 16:19:33 +0200

    done

--- js/script.js
+++ js/script.js
@@ -4,5 +4,6 @@ var cognome = prompt('Qual è il tuo cogmome?');
 console.log(cognome);
 var colore = prompt('Qual è il tuo colore preferito?')
 console.log(colore);
-var psw = nome + cognome + colore;
+var stringaPsw = 'La tua password è ' + nome + cognome + colore + '19';
 console.log(psw);
+document.getElementById("psw").innerHTML = stringaPsw;
