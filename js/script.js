d1300e3aac646877933bee2a1f904d65aa602b69
francesca-campana <franx3rox@gmail.com>
2020-05-19 16:03:49 +0200

    wip

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var nome = prompt('Come ti chiami?');
+console.log(nome);
+var cognome = prompt('Qual è il tuo cogmome?');
+console.log(cognome);
+var colore = prompt('Qual è il tuo colore preferito?')
+console.log(colore);
+var psw = nome + cognome + colore;
+console.log(psw);
