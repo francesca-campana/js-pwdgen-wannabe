f85b8293a598aec6afc44c4447bdf6de88fee639
francesca-campana <franx3rox@gmail.com>
2020-05-19 16:32:39 +0200

    prova con math.floor

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var cognome = prompt('Qual è il tuo cogmome?');
 console.log(cognome);
 var colore = prompt('Qual è il tuo colore preferito?')
 console.log(colore);
-var stringaPsw = 'La tua password è ' + nome + cognome + colore + '19';
+var stringaPsw = 'La tua password è ' + nome + cognome + colore;
+console.log(stringaPsw);
+document.getElementById("psw").innerHTML = stringaPsw + Math.floor(Math.random(18) * 20);
 console.log(psw);
-document.getElementById("psw").innerHTML = stringaPsw;
